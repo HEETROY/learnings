e851c8b7b6b075f573e4eb32ec699bcc5b78879f
Heet Roy <heetroy@gmail.com>
2020-03-04 12:29:54 +0530

    Shree Ganesh

--- sql/oops/class.php
+++ sql/oops/class.php
@@ -9,9 +9,15 @@ class Car{
         $this->doors = $x;
     }
 }
+class Plane extends Car{
+    //we inherited the properties of class car to class plane using the keyword extends
+}
 
 $bmw = new Car();
+$jet = new Plane();
 $bmw->createdoors(4);
 echo "created ".$bmw->doors ." doors"."<br>";
 echo 'created '.$bmw->engine =1 . ' engine'.'<br>';
+$jet->createdoors(2);
+echo " created " . $jet->doors . " doors in plane using inheritance"."<br>";
 ?>
